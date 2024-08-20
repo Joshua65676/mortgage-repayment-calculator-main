28915998d518292e53c4c845d9fd1d01c8b68303
Okoro Joshua <Joshua65676@gmail.com>
2024-08-20 09:18:14 +0100

    first commit

--- src/conpomnent/emptyDesign.tsx
+++ src/conpomnent/emptyDesign.tsx
@@ -3,8 +3,8 @@ import Form from "./form"
 const EmptyDesign = () => {
 
   return (
-    <div className="flex">
-      <div className="bg-White md:w-5/12 sm:-ml-9 md:mt-10 md:h-max md:rounded-3xl md:ml-52 -mt-7 sm:h-[700px] sm:w-[390px]">
+    <div className="flex md:-ml-10 sm:ml-0">
+      <div className="bg-White md:w-9/12 sm:-ml-9 md:mt-10 md:h-max md:rounded-3xl md:ml-52 -mt-7 sm:h-[700px] sm:w-[390px]">
         <div className=" p-10 -mb-1 sm:-ml-3 md:ml-0">
             <h1 className="text-xl font-bold text-Slate-900">Mortgage Calculator</h1>
         </div>
